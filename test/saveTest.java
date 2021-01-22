9480ce787cad5edd3e8cac3182939bb8f5f16a67
JuanGalicia20 <69065800+JuanGalicia20@users.noreply.github.com>
2021-01-22 03:19:08 -0600

    Pruebas unitarias iniciales

---
+++ test/saveTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import radio.Radio;
+
+class saveTest {
+
+	@Test
+	void test() {
+        Radio testGuardar = new Radio();
+        testGuardar.encenderApagar();
+        String avanzar = testGuardar.guardar(1);
+        assertEquals("Se ha guardado la emisora: 87.9 en el boton: #1",avanzar);
+	}
+
+}
